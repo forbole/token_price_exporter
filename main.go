0e518f8bd8d7730c65a6e5a7fffe8478ab3a0f81
Mai Quang Hiep <maiquanghiepdl@gmail.com>
2022-09-27 03:09:53 +0000

    Get token prices every 5 mintues

--- main.go
+++ main.go
@@ -5,20 +5,29 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
-	collector "github.com/forbole/token-price-exporter/collectors"
 	token "github.com/forbole/token-price-exporter/types"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	coingecko "github.com/superoo7/go-gecko/v3"
 )
 
 var (
 	// Used for flags
-	cfgFile string
-	port    string
-	Config  token.Config
+	cfgFile            string
+	port               string
+	Config             token.Config
+	tokenPriceGaugeVec = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "token_price",
+			Help: "Price of the token",
+		},
+		[]string{"token_id", "denom"},
+	)
 )
 
 func NewRootCommand() *cobra.Command {
@@ -53,17 +62,10 @@ func NewRootCommand() *cobra.Command {
 
 func Executor(cmd *cobra.Command, args []string) error {
 	tokens := Config.Tokens
-	registry := prometheus.NewPedanticRegistry()
-	registry.MustRegister(
-		collector.NewTokensPriceGauge(tokens),
-	)
+	var tokenPrice *map[string]map[string]float32
+	getPrice(tokens, tokenPrice)
 
-	handler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{
-		ErrorLog:      log.New(os.Stderr, log.Prefix(), log.Flags()),
-		ErrorHandling: promhttp.ContinueOnError,
-	})
-
-	http.Handle("/metrics", handler)
+	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(port, nil))
 	fmt.Printf("Start listening on port %s", port)
 	return nil
@@ -77,4 +79,32 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func getPrice(tokens []token.Token, tokenPrice *map[string]map[string]float32) {
+	go func() {
+		for {
+			httpClient := &http.Client{
+				Timeout: time.Second * 10,
+			}
+
+			cg := coingecko.NewClient(httpClient)
+			ids := []string{}
+			for _, token := range tokens {
+				ids = append(ids, token.ID)
+			}
+
+			vc := []string{"usd"}
+
+			sp, err := cg.SimplePrice(ids, vc)
+			if err == nil {
+				for _, token := range tokens {
+					if tokenPrice, ok := (*sp)[token.ID]; ok {
+						tokenPriceGaugeVec.WithLabelValues(token.ID, token.Denom).Set(float64(tokenPrice["usd"]))
+					}
+				}
+			}
+			time.Sleep(5 * time.Minute)
+		}
+	}()
 }
